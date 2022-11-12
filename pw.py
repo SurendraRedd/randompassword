6d29c8fe45e76a5788c975f77529758879a4574f
Surendra Reddy <surendraelectronics@gmail.com>
2022-11-12 18:24:58 +0000

    initial development

--- pw.py
+++ pw.py
@@ -1,6 +1,6 @@
 '''
 Random Password Generator using Python
-Author: Ayushi Rawat
+Author: Surendra Reddy
 '''
 
 #import the necessary modules!
@@ -10,22 +10,24 @@ import string
 
 from PIL import Image
 
-
-
-st.title("🔑 Strong Password Generator")
-image = Image.open('https://www.technologyblog.org/wp-content/uploads/2022/10/complex-password-generator.jpg')
+st.title("🔑 Random Password Generator")
+image = Image.open('password-generator.jpg')
 st.image(image, caption='Random Password Generator')
 
-with st.expander("Check explanation below"):
-    st.markdown('Strong **Password Generator** to generate secure passwords from characters, \
+with st.expander("👉 Explanation"):
+    st.markdown('**Random Password Generator** to generate secure passwords from characters, \
                 letters, numbers, symbols, and special characters. Random password generator to create \
-                alphanumeric passwords for any kind of login or other uses.')
-#print('hello, Welcome to Password generator!')
+                alphanumeric passwords for any kind of login or other uses')
+    st.markdown('- 👉Select the **length** of the password')
+    st.markdown('- 👉**Random password** will be generated based on the length')
+    st.write('\n')
 
-#input the length of password
-#length = int(input('\nEnter the length of password: '))
-lengthst = int(st.number_input('Enter the length of password:',0,34))
-#st.write(lengthst)                      
+# Tooltips also support markdown
+input_markdown = '''
+By default max length will be 34 and min length is 0!
+'''.strip()
+lengthst = int(st.number_input('Enter the length of password',0,34,help=input_markdown))
+                  
 
 #define data
 lower = string.ascii_lowercase
@@ -38,13 +40,13 @@ symbols = string.punctuation
 all = lower + upper + num + symbols
 
 #use random 
-#temp = random.sample(all,length)
 temp1 = random.sample(all,lengthst)
 
 #create the password 
-#password = "".join(temp)
 password1 = "".join(temp1)
 
 #print the password
-#print(password)
 st.text_area('Random Password:',password1)
+
+with st.expander('Share This Tool'):
+    st.text_area('',password1)
